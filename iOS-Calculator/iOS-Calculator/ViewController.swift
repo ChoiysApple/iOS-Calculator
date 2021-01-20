a155cd285726cef26369b17e3b30ed225a50f6a9
ChoiysApple <chcs1370@gmail.com>
2021-01-20 17:36:06 +0900

    Make buttons round

--- iOS-Calculator/iOS-Calculator/ViewController.swift
+++ iOS-Calculator/iOS-Calculator/ViewController.swift
@@ -9,9 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- IBOutlets
+    
+    // result label
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // function button
+    @IBOutlet weak var ACButton: UIButton!
+    @IBOutlet weak var posNegButton: UIButton!
+    @IBOutlet weak var modulatButton: UIButton!
+    
+    // operation button
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak var addbutton: UIButton!
+    @IBOutlet weak var resultbutton: UIButton!
+    
+    // number button
+    @IBOutlet weak var buttonDot: UIButton!
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let buttons = [ACButton, posNegButton, modulatButton, divideButton, multiplyButton, subtractButton, addbutton, resultbutton, buttonDot, button1, button2, button3, button4, button5, button6, button7, button8, button9]
+        
+        // make buttons round
+        for button in buttons {
+            if let btn = button {
+                btn.layer.cornerRadius = btn.bounds.width / 2
+            }
+            button0.layer.cornerRadius = button0.bounds.height / 2
+            
+        }
     }
 
 
