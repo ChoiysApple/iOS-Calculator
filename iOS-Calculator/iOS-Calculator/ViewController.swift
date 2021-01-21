7ffd80c150203ff2b37264fff4d7c19d2db41a1a
ChoiysApple <chcs1370@gmail.com>
2021-01-21 14:10:14 +0900

    Revert

--- iOS-Calculator/iOS-Calculator/ViewController.swift
+++ iOS-Calculator/iOS-Calculator/ViewController.swift
@@ -39,40 +39,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var button8: UIButton!
     @IBOutlet weak var button9: UIButton!
     
-    // Additional button
-    @IBOutlet weak var openBraceButton: UIButton!
-    @IBOutlet weak var closeBraceButton: UIButton!
-    @IBOutlet weak var mcButton: UIButton!
-    @IBOutlet weak var mPButton: UIButton!
-    @IBOutlet weak var mNButton: UIButton!
-    @IBOutlet weak var mrButton: UIButton!
-    @IBOutlet weak var secondButton: UIButton!
-    @IBOutlet weak var squareButton: UIButton!
-    @IBOutlet weak var thirdPowerButton: UIButton!
-    @IBOutlet weak var yPowerButton: UIButton!
-    @IBOutlet weak var ePowerButton: UIButton!
-    @IBOutlet weak var tenPowerButton: UIButton!
-    @IBOutlet weak var divideByButton: UIButton!
-    @IBOutlet weak var root2Button: UIButton!
-    @IBOutlet weak var root3Button: UIButton!
-    @IBOutlet weak var rootYButton: UIButton!
-    @IBOutlet weak var lnButton: UIButton!
-    @IBOutlet weak var log10Button: UIButton!
-    @IBOutlet weak var pactorialButton: UIButton!
-    @IBOutlet weak var sinButton: UIButton!
-    @IBOutlet weak var cosButton: UIButton!
-    @IBOutlet weak var tanButton: UIButton!
-    @IBOutlet weak var eButton: UIButton!
-    @IBOutlet weak var EEButton: UIButton!
-    @IBOutlet weak var RadButton: UIButton!
-    @IBOutlet weak var sinhButton: UIButton!
-    @IBOutlet weak var coshButton: UIButton!
-    @IBOutlet weak var tanhButton: UIButton!
-    @IBOutlet weak var piButton: UIButton!
-    @IBOutlet weak var RadhButton: UIButton!
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -81,13 +47,14 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         
-        let buttons = [ACButton, posNegButton, modulatButton, divideButton, multiplyButton, subtractButton, addbutton, resultbutton, buttonDot, button0, button1, button2, button3, button4, button5, button6, button7, button8, button9]
+        let buttons = [ACButton, posNegButton, modulatButton, divideButton, multiplyButton, subtractButton, addbutton, resultbutton, buttonDot, button1, button2, button3, button4, button5, button6, button7, button8, button9]
         
         // make buttons round
         for button in buttons {
             if let btn = button {
-                btn.layer.cornerRadius = btn.bounds.height / 2
+                btn.layer.cornerRadius = btn.bounds.width / 2
             }
+            button0.layer.cornerRadius = button0.bounds.height / 2
             
         }
     }
